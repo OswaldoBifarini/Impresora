f273b22889e7f65ef3bff245a547b390f5ec7e0e
OswaldoBifarini <jgbifarini@gmail.com>
2024-10-30 15:21:32 -0500

    comentario

---
+++ src/clasepoo30_10_24/ClasePoo30_10_24.java
@@ -0,0 +1,62 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package clasepoo30_10_24;
+
+/**
+ *
+ * @author DELL
+ */
+public class ClasePoo30_10_24 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        /*
+            se compone de estos 4 elementos 
+            OBJETO ES LA ABSTRACCION DE ALGO EN EL MUNDO REAL
+        propiedades
+        metodos = funcionalidades 
+        
+        la orientacion a objetos es  observar porblemas en forma de objetos
+        
+                CLASE 
+        it is the model on wich an object will be build, tjat is , they
+        templates tjat contain tje basic
+        
+            COMO CREAR UNA CLASE
+        nombreClase
+        atributo1           identidad
+        atributon           Estadi
+        operacion 1         comportamiento
+        operacionn
+     
+        
+       "DIAGRAMAR UNA CLASE" DIAGRAMADO DE UNA CLASE
+        
+        ANALIZAR, TESTEAR
+        
+        alumno (ATRIBUTOS clave_al; nombre; apellido; n creditos)(FUNCIONALIDADES O METODOS
+        pagar ; retirarse : aplazar)
+        
+        programa(ATRIBUTOS claveCurso; nombre; duracion; costo; aula  )(FUNCIONALIDADES O METODOS
+        seleccionarAlumnos; cancelarCurso)
+        
+        +clases.- mode q permite crear objets
+        +propiedades
+        +metodos
+        +objetos
+        
+            tiene estos pilares
+        +encapsulado
+        +abstraccion
+        +herencia
+        +polimorfismo
+        
+        */
+    }
+    
+}
